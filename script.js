d937bae2c7cbdf04adc38b8be02932f043d7af45
MinhHien <113273285+mink-hynw@users.noreply.github.com>
2022-12-19 23:33:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,19 @@ const numbers = document.getElementsByClassName("btn");
 const result = document.getElementById("result");
 
 for (let number of numbers) {
-    result.innerHTML = "0";
+    
   number.addEventListener("click", () => {
     result.innerHTML += number.value;
   });
 }
 
 function clean() {
-  result.innerHTML = "0";
+  result.innerHTML = "";
 }
 
 function undo() {
-
   let res = result.innerHTML;
-  
-  if(res.length == 1){
-    result.innerHTML = "0";
-  } else{
-    result.innerHTML = res.substring(0, res.length - 1);
-  }
+  result.innerHTML = res.substring(0, res.length - 1);
 }
 
 function equal() {
